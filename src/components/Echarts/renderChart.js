3521ffb2779e0e2610804141f6eabfed3543f152
liulujia <liulujia@jimi360.cn>
2019-04-22 15:11:49 +0800

    改变替换字符规则

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -4,12 +4,21 @@ import toString from '../../util/toString';
 export default function renderChart(props) {
   const height = `${props.height || 400}px`;
   const width = props.width ? `${props.width}px` : 'auto';
-  const selectXName = props.selectXName ? props.selectXName : ""
+  // const selectXName = props.selectXName ? props.selectXName : ""
+  const selectObject = props.selectObject
+
   var optionS = props.option
   let strOpens = toString(optionS)
-  if (selectXName) {
-    strOpens = strOpens.replace("!@#$%^&%#",selectXName)
+  if (selectObject) {
+    for ( const key in selectObject){
+      strOpens = strOpens.replace(key,selectObject[key])
+    }
   }
+  // if (selectXName) {
+  //   strOpens = strOpens.replace("!@#$%^&%#",selectXName)
+  // }
+
+  
   return `
     document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
